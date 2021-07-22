9c645c59c1ca5df4798068bc492b1d291899083e
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-07-22 10:54:05 +0530

    file organized

---
+++ Controllers/OwnerController/OwnerController.cs
@@ -0,0 +1,35 @@
+﻿using Foodilizer_Group35.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodilizer_Group35.Controllers
+{
+    public class OwnerController : Controller
+    {
+        private readonly ILogger<OwnerController> _logger;
+
+        public OwnerController(ILogger<OwnerController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Owner_home()
+        {
+            return View();
+        }
+
+       
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
