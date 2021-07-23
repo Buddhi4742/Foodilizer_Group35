bf2d153b0add0d1f6bfd92724e69e47895f83ea9
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-07-23 10:53:57 +0530

    sect 2 and pack layout

--- wwwroot/js/owner_home.js
+++ wwwroot/js/owner_home.js
@@ -24,26 +24,4 @@ navBar.forEach(function (a) {
     })
 })
 
-// counter design
-document.addEventListener("DOMContentLoaded", () => {
-    function counter(id, start, end, duration) {
-        let obj = document.getElementById(id),
-            current = start,
-            range = end - start,
-            increment = end > start ? 1 : -1,
-            step = Math.abs(Math.floor(duration / range)),
-            timer = setInterval(() => {
-                current += increment;
-                obj.textContent = current;
-                if (current == end) {
-                    clearInterval(timer);
-                }
-            }, step);
-    }
-    counter("count1", 0, 1287, 3000);
-    counter("count2", 100, 5786, 2500);
-    counter("count3", 0, 1440, 3000);
-    counter("count4", 0, 7110, 3000);
-});
-
 
