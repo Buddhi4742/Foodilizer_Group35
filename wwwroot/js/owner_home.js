ebf0b7bd6ba89e28ba595459d5a48cdb999aab1c
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-07-26 21:07:45 +0530

    register layout 1

--- wwwroot/js/owner_home.js
+++ wwwroot/js/owner_home.js
@@ -24,4 +24,14 @@ navBar.forEach(function (a) {
     })
 })
 
+//preview image 
+function getImagePreview(event) {
+    var image = URL.createObjectURL(event.target.files[0]);
+    var imagediv = document.getElementById('preview');
+    var newimg = document.createElement('img');
+    imagediv.innerHTML = '';
+    newimg.src = image;
+    newimg.width = "300";
+    imagediv.appendChild(newimg);
+}
 
