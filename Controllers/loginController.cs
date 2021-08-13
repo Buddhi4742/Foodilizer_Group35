7c74bdeb66e1d4705aba671bc94c9576b396500e
osanda bhanuranga rathnayaka <osandabhanuranga@gmail.com>
2021-08-13 15:20:41 +0530

    signup edits

---
+++ Controllers/loginController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Foodilizer_Group35.Controllers
+{
+    public class loginController : Controller
+    {
+        // GET: loginController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: loginController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: loginController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: loginController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: loginController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: loginController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: loginController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: loginController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
