6f9a6ed6160a8fcd9dc822de30205f6f30cc9bcc
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-08-20 18:05:17 +0530

    register view create

---
+++ Controllers/OwnerController.cs
@@ -0,0 +1,57 @@
+﻿using Foodilizer_Group35.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Foodilizer_Group35.Controllers
+{
+    public class OwnerController : Controller
+    {
+      
+
+        // GET: OwnerController
+        public ActionResult owner_home()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            using (foodilizerContext dbmodel = new foodilizerContext())
+                return View(dbmodel.Restaurants.ToList());
+        }
+
+
+
+        // GET: OwnerController/Create
+        public ActionResult owner_register()
+        {
+            return View();
+        }
+
+        // POST: OwnerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            // try
+            //{
+            using (foodilizerContext dbmodel = new foodilizerContext())
+            {
+                dbmodel.Restaurants.Add(restaurant);
+                dbmodel.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+          //  }
+          //  catch
+            //{
+              //  return View();
+            //}
+        }
+
+        
+    }
+}
