9ccf0f1b9678521d417925f0add88e595a29a7de
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-08-24 10:32:13 +0530

    controller add

--- Controllers/OwnerController.cs
+++ Controllers/OwnerController.cs
@@ -7,27 +7,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
+using Microsoft.AspNetCore.Mvc.Filters;
+
 namespace Foodilizer_Group35.Controllers
 {
     public class OwnerController : Controller
     {
-      
+        private readonly foodilizerContext _context;
+        public OwnerController(foodilizerContext context)
+        {
+            _context = context;
+        }
 
         // GET: OwnerController
-        public ActionResult owner_home()
+        public IActionResult owner_home()
         {
             return View();
         }
-        public ActionResult Index()
+
+        public IActionResult Index()
         {
-            using (foodilizerContext dbmodel = new foodilizerContext())
-                return View(dbmodel.Restaurants.ToList());
+            return View();
         }
 
-      
-
         // GET: OwnerController/Create
-        public ActionResult owner_register1()
+        public IActionResult owner_register()
         {
             return View();
         }
@@ -35,23 +39,34 @@ namespace Foodilizer_Group35.Controllers
         // POST: OwnerController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Restaurant restaurant)
+        public async Task<IActionResult> Owner_register([Bind("Rname,Owner_name,Owner_contact,Owner_email,Rabout,Rest_type,Remail,Raddress,Rdistrict,Price_range,Rusername,Rpassword,Rprovince,Open_hour,Open_status,Website_link,Map_link,Meal_type,Cuisine,Feature,Special_diet")] Restaurant restaurant)
         {
-            try
+            if (ModelState.IsValid)
             {
-            using (foodilizerContext dbmodel = new foodilizerContext())
-            {
-                dbmodel.Restaurants.Add(restaurant);
-                dbmodel.SaveChanges();
+                
+
+
+                var updateuser = new User();
+                updateuser.UserId = restaurant.RestId;
+                updateuser.Email = restaurant.Remail;
+                updateuser.Password =restaurant.Rpassword;
+                updateuser.UserType = "REST";
+                _context.Add(updateuser);
+                _context.Add(restaurant);
+
+                await _context.SaveChangesAsync();
+
+                TempData["Message"] = "User registered.";
+                return RedirectToAction(nameof(Index));
+
             }
-            return RedirectToAction(nameof(Index));
-           }
-           catch
-            {
-                return View();
-            }
+            ViewBag.Error = "Unable to register this user. Please try agian";
+            return View(restaurant);
         }
 
-        
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.UserId == id);
+        }
     }
 }
