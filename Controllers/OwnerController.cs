70826a405ea666534605dce4d219cce06b938d19
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-08-27 18:22:25 +0530

    3 table model created

--- Controllers/OwnerController.cs
+++ Controllers/OwnerController.cs
@@ -31,6 +31,8 @@ namespace Foodilizer_Group35.Controllers
             return View();
         }
         private readonly foodilizerContext _context;
+
+
         public OwnerController(foodilizerContext context)
         {
             _context = context;
@@ -44,46 +46,78 @@ namespace Foodilizer_Group35.Controllers
         // POST: OwnerController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Owner_register([Bind("Rname,Owner_name,Owner_contact,Owner_email,Rabout,Rest_type,Remail,Raddress,Rdistrict,Price_range,Rusername,Rpassword,Rprovince,Open_hour,Open_status,Website_link,Map_link,Meal_type,Cuisine,Feature,Special_diet")] Restaurant restaurant)
+        public async Task<IActionResult> Owner_register([Bind("Rname,Owner_name,Owner_contact,Owner_email,Rabout,Rest_type,Remail,Raddress,Rdistrict,Price_range,Rusername,Rpassword,Rprovince,Open_hour,Open_status,Website_link,Map_link,Meal_type,Cuisine,Feature,Special_diet,contact_number,main_image_path")] RestRegis restRegis)
         {
             if (ModelState.IsValid)
             {
                 try {
-                    var restDetails = await _context.Restaurants.FirstOrDefaultAsync(e => e.Remail == restaurant.Remail);
+                  var  updaterest  = new Restaurant();
+                    var restDetails = await _context.Restaurants.FirstOrDefaultAsync(e => e.Remail == restRegis.Remail);
 
                     if (restDetails != null)
                     {
                         TempData["Error"] = "A User with the email already exists. Did you meant to Sign in?";
                         return RedirectToAction("Owner_register");
                     }
-                    //   var updateuser = new User();
-                 
-                    restaurant.ShaEnc();
+                     var updateuser = new User();
+                    var updatecontact = new RestaurantContact();
+                    var updateimage = new RestaurantImage();
+                    restRegis.ShaEnc();
                     
-             //   updateuser.UserId = restaurant.RestId;
-             //   updateuser.Email = restaurant.Remail;
-              //  updateuser.Password =restaurant.Rpassword;
-              //  updateuser.UserType = "REST";
-              //  _context.Add(updateuser);
-                _context.Add(restaurant);
+                updateuser.UserId = restRegis.RestId;
+                updateuser.Email = restRegis.Remail;
+                updateuser.Password =restRegis.Rpassword;
+               updateuser.UserType = "REST";
+                  
+               _context.Add(updateuser);
+                    updaterest.Cuisine = restRegis.Cuisine;
+                    updaterest.Feature = restRegis.Feature;
+                    updaterest.MapLink = restRegis.MapLink;
+                    updaterest.MealType = restRegis.MealType;
+                    updaterest.OpenHour = restRegis.OpenHour;
+                    updaterest.OwnerContact = restRegis.OwnerContact;
+                    updaterest.OwnerEmail = restRegis.OwnerEmail;
+                    updaterest.OwnerName = restRegis.OwnerName;
+                    updaterest.PriceRange = restRegis.PriceRange;
+                    updaterest.Rabout = restRegis.Rabout;
+                    updaterest.SpecialDiet = restRegis.SpecialDiet;
+                    updaterest.Raddress = restRegis.Raddress;
+                    updaterest.Rdistrict = restRegis.Rdistrict;
+                    updaterest.Remail = restRegis.Remail;
+                    updaterest.RestType = restRegis.RestType;
+                    updaterest.Rname = restRegis.Rname;
+                    updaterest.Rpassword = restRegis.Rpassword;
+                    updaterest.Rprovince = restRegis.Rprovince;
+                    updaterest.Rusername = restRegis.Remail;
+                    updaterest.WebsiteLink = restRegis.WebsiteLink;
 
-                await _context.SaveChangesAsync();
+                    _context.Add(updaterest);
+                    updatecontact.RestId = restRegis.RestId;
+                    updatecontact.ContactNumber = restRegis.ContactNumber;
+                    _context.Add(updatecontact);
+
+                    updateimage.MainImagePath = restRegis.MainImagePath;
+                    updateimage.RestId = restRegis.RestId;
+                    _context.Add(updateimage);
+
+
+                    await _context.SaveChangesAsync();
 
                 TempData["Message"] = "User registered";
-                return RedirectToAction(nameof(Owner_price));
+                return RedirectToAction(nameof(Owner_register));
 
                  }
                 catch (Exception ex)
                 {
                     if (ex.InnerException.ToString().Contains("Violation of UNIQUE KEY constraint")) ViewBag.Error = "This email is already used. Please use another email address";
                     else ViewBag.Error = "Unable to register this user. Please try agian";
-                    return View(restaurant);
+                    return View(restRegis);
 
                 }
 
             }
             ViewBag.Error = "Unable to register this user. Please try agian";
-            return View(restaurant);
+            return View(restRegis);
         }
 
         private bool UserExists(int id)
---
+++ Models/RestRegis.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodilizer_Group35.Models
+{
+    public class RestRegis
+    {
+      
+        public int RestId { get; set; }
+        
+        public string Rname { get; set; }
+       
+        public string OwnerName { get; set; }
+     
+        public string OwnerContact { get; set; }
+       
+        public string OwnerEmail { get; set; }
+      
+        public string Rabout { get; set; }
+        
+        public string RestType { get; set; }
+      
+        public string Remail { get; set; }
+        
+        public string Raddress { get; set; }
+        
+        public string Rdistrict { get; set; }
+       
+        public string PriceRange { get; set; }
+    
+        public string Rusername { get; set; }
+    
+        public string Rpassword { get; set; }
+  
+   
+        public string Rprovince { get; set; }
+     
+        public string OpenHour { get; set; }
+       
+        public sbyte? OpenStatus { get; set; }
+     
+        public string WebsiteLink { get; set; }
+ 
+        public string MapLink { get; set; }
+  
+        public string MealType { get; set; }
+
+        public string Cuisine { get; set; }
+ 
+        public string Feature { get; set; }
+   
+        public string SpecialDiet { get; set; }
+
+        public string ContactNumber { get; set; }
+
+        public string MainImagePath { get; set; }
+
+        public void ShaEnc()
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(Rpassword));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                Rpassword = builder.ToString();
+            }
+        }
+
+
+
+
+
+
+    }
+}
