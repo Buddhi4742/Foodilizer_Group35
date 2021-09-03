501601a9bfcf88e3e052e90baa2687f6274a2064
osanda bhanuranga rathnayaka <osandabhanuranga@gmail.com>
2021-09-03 15:58:04 +0530

    updates

--- Controllers/StoreAdminController.cs
+++ Controllers/StoreAdminController.cs
@@ -14,7 +14,42 @@ namespace Foodilizer_Group35.Controllers
         {
             return View();
         }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult Menu()
+        {
+            return View();
+        }
+        public ActionResult Inventory()
+        {
+            return View();
+        }
+        public ActionResult Recomendations()
+        {
+            return View();
+        }
+        public ActionResult Banner()
+        {
+            return View();
+        }
+        public ActionResult Orders()
+        {
+            return View();
+        }
+        public ActionResult OtherUsers()
+        {
+            return View();
+        }
+        public ActionResult Reports()
+        {
+            return View();
+        }
+        public ActionResult Inspect()
+        {
+            return View();
+        }
 
-        
     }
 }
