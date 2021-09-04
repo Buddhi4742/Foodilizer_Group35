5d5f76ecac48b13f6eed986e21fb5934cd919b27
Buddhi4742 <chathuranga4742@gmail.com>
2021-09-04 20:05:20 +0530

    commit

--- Controllers/OwnerController.cs
+++ Controllers/OwnerController.cs
@@ -11,7 +11,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 //using Microsoft.AspNetCore.Mvc.Filters;
 //using Microsoft.Graph;
-
+using System.Security.Cryptography;
+using System.Text;
 namespace Foodilizer_Group35.Controllers
 {
     public class OwnerController : Controller
@@ -41,15 +42,80 @@ namespace Foodilizer_Group35.Controllers
         {
             return View();
         }
-        public IActionResult RegisterDetails()
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>RegisterDetails(IFormCollection collection)
         {
-            return View();
+            
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //await Response.WriteAsync(collection["Remail"].ToString());
+                    var restdetails = await _context.Restaurants.FirstOrDefaultAsync(e => e.Remail == collection["Remail"].ToString() /*&& e.User_status == 1*/);
+
+                    if (restdetails != null)
+                    {
+                        //await Response.WriteAsync(collection["Remail"].ToString() + "Already have");
+
+                        TempData["Error"] = "A User with the email already exists. Did you meant to Sign in?";
+                        return RedirectToAction("Index");
+                    }
+
+                    /*await Response.WriteAsync(collection["Remail"].ToString() + "can register");*/
+                    var updateuser = new User();
+                    //updateuser.UserStatus = 1;
+                    //customer.ShaEnc();
+                    string encpass = ShaEnc(collection["Rpassword"].ToString());
+                     //await _context.SaveChangesAsync();
+
+
+                     //updateuser.UserId = customer.CustomerId;
+                     //updateuser.Email = customer.Cemail;
+                     //updateuser.Password = customer.Password;
+                     //updateuser.UserType = "CUST";
+                     //_context.Add(updateuser);
+                     //_context.Add(customer);
+
+                     await _context.SaveChangesAsync();
+
+                    TempData["Message"] = "User registered.";
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (Exception ex)
+                {
+                    if (ex.InnerException.ToString().Contains("Violation of UNIQUE KEY constraint")) ViewBag.Error = "This email is already used. Please use another email address";
+                    else ViewBag.Error = "Unable to register this user. Please try agian";
+                    return View(/*customer*/);
+
+                }
+
+            }
+            ViewBag.Error = "Unable to register this user. Please try agian";
+            return View(/*customer*/);
+           
         }
         
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public string ShaEnc(string Rpassword)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(Rpassword));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                Rpassword = builder.ToString();
+                return Rpassword;
+            }
         }
     }
 }
