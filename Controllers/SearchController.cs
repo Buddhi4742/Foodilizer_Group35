644b33e98868e4fafe8b924062a6a63f4e1fb73a
Buddhi4742 <chathuranga4742@gmail.com>
2021-09-23 05:44:41 +0530

    commit

--- Controllers/SearchController.cs
+++ Controllers/SearchController.cs
@@ -140,6 +140,27 @@ namespace Foodilizer_Group35.Controllers
                 return RedirectToAction("Index","Home");
             }
         }
+        public IActionResult redirectstorewithfoods(int id)
+        {
+            var resttype = _context.Restaurants.Where(x => x.RestId == id).FirstOrDefault();
+
+            if (resttype.RestType == "bronze")
+            {
+                return RedirectToAction("bronze_home", "store_front", new { id });
+            }
+            else if (resttype.RestType == "silver")
+            {
+                return RedirectToAction("silver_home", "store_front", new { id });
+            }
+            else if (resttype.RestType == "gold")
+            {
+                return RedirectToAction("gold_home", "store_front", new { id });
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
 
         //// GET: SearchController/Details/5
         //public IActionResult FoodDetails(int id)
